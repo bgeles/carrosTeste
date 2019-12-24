c726a7673eb8b656b2dcb316ac5a27e2e6a014d2
Bruno Glauco Eles <bgeles@gmail.com>
2019-12-24 16:24:02 -0300

    adicionando http

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -1,6 +1,10 @@
+import 'package:carros/pages/home_page.dart';
+import 'package:carros/utils/nav.dart';
 import 'package:carros/widgets/app_button.dart';
 import 'package:carros/widgets/app_text.dart';
 import 'package:flutter/material.dart';
+
+import 'login_api.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -34,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
         child: ListView(
           children: <Widget>[
             AppText(
-              "Logi",
+              "Login",
               "Digite seu login",
               controller: _tLogin,
               validator: _validateLogin,
@@ -80,53 +84,7 @@ class _LoginPageState extends State<LoginPage> {
     return null;
   }
 
-  _text(String label, String hint,
-      {bool password = false,
-      TextEditingController controller,
-      FormFieldValidator<String> validator,
-      TextInputType keyboardType,
-      TextInputAction textInputAction,
-      FocusNode focusNode,
-      FocusNode nextFocus}) {
-    return TextFormField(
-      controller: controller,
-      obscureText: password,
-      validator: validator,
-      keyboardType: keyboardType,
-      textInputAction: textInputAction,
-      focusNode: focusNode,
-      onFieldSubmitted: (String text) {
-        if (nextFocus != null) {
-          FocusScope.of(context).requestFocus(nextFocus);
-        }
-      },
-      style: TextStyle(
-        fontSize: 25,
-        color: Colors.blue,
-      ),
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(
-          fontSize: 25,
-          color: Colors.grey,
-        ),
-        hintText: hint,
-        hintStyle: TextStyle(
-          fontSize: 16,
-        ),
-      ),
-    );
-  }
-
-/*
-  _button(String text, Function onPressed) {
-    AppButton(
-      "Login",
-      _onClickLogin,
-    );
-  }
-*/
-  void _onClickLogin() {
+  Future<void> _onClickLogin() async {
     if (!_formKey.currentState.validate()) {
       return;
     }
@@ -135,5 +93,15 @@ class _LoginPageState extends State<LoginPage> {
     String senha = _tSenha.text;
 
     print("login: $login, Senha: $senha");
+
+    bool ok = await LoginApi.login(login, senha);
+
+    print('OK -> $ok');
+
+    if (ok) {
+      push(context, HomePage());
+    } else {
+      print('Login incorreto');
+    }
   }
 }
