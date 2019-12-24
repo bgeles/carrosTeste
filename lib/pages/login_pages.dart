34560246cd51a3dbf541f0c3aaa02fbf6c9455b8
Bruno Glauco Eles <bgeles@gmail.com>
2019-12-24 11:38:24 -0300

    Commit widgets text e button

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -1,6 +1,21 @@
+import 'package:carros/widgets/app_button.dart';
+import 'package:carros/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _tLogin = TextEditingController();
+
+  final _tSenha = TextEditingController();
+
+  final _focusSenha = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,37 +27,79 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: <Widget>[
-          _text("Login", "Digite seu login"),
-          SizedBox(
-            height: 10,
-          ),
-          _text("Senha", "Digite sua senha", password: true),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 46,
-            child: RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Login",
-                style: TextStyle(color: Colors.white, fontSize: 22),
-              ),
-              onPressed: () {},
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            AppText(
+              "Logi",
+              "Digite seu login",
+              controller: _tLogin,
+              validator: _validateLogin,
+              textInputAction: TextInputAction.next,
+              nextFocus: _focusSenha,
             ),
-          ),
-        ],
+            SizedBox(
+              height: 10,
+            ),
+            AppText("Senha", "Digite sua senha",
+                controller: _tSenha,
+                password: true,
+                validator: _validateSenha,
+                keyboardType: TextInputType.number,
+                focusNode: _focusSenha),
+            SizedBox(
+              height: 20,
+            ),
+            AppButton(
+              "Login",
+              onPressed: _onClickLogin,
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  TextFormField _text(String label, String hint, {bool password = false}) {
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return "Digite o login";
+    }
+    return null;
+  }
+
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
+      return "Digite o senha";
+    }
+    if (text.length < 3) {
+      return "Senha precisa no minimo 3 caracteres";
+    }
+    return null;
+  }
+
+  _text(String label, String hint,
+      {bool password = false,
+      TextEditingController controller,
+      FormFieldValidator<String> validator,
+      TextInputType keyboardType,
+      TextInputAction textInputAction,
+      FocusNode focusNode,
+      FocusNode nextFocus}) {
     return TextFormField(
+      controller: controller,
       obscureText: password,
+      validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: (String text) {
+        if (nextFocus != null) {
+          FocusScope.of(context).requestFocus(nextFocus);
+        }
+      },
       style: TextStyle(
         fontSize: 25,
         color: Colors.blue,
@@ -59,5 +116,24 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+/*
+  _button(String text, Function onPressed) {
+    AppButton(
+      "Login",
+      _onClickLogin,
+    );
+  }
+*/
+  void _onClickLogin() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("login: $login, Senha: $senha");
   }
 }
